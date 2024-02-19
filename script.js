5580fd8a2a9900e4180208549bc353e8f55c8930
Nihalp01 <nihalburagohain02@gmail.com>
2024-02-19 09:22:00 +0530

    some tips added !

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 async function getData() {
-  const data = document.getElementById("inputvalue");
+  const data = document.getElementById('inputvalue');
 
   var userName = data.value;
+  // TODO:Instead of this alert, use input error. Search in google about how to show error in the input field
   if (!data.value) {
-    alert("Enter Gihub Username")
-    return
+    alert('Enter Gihub Username');
+    return;
   }
-  var ApiUrl = "https://api.github.com/users/" + userName;
+  var ApiUrl = 'https://api.github.com/users/' + userName;
   // console.log(ApiUrl)
 
   const value = await fetch(ApiUrl);
   const response = await value.json();
 
-  
-  console.log(response)
+  console.log(response);
   const locationElement = document.querySelector('.locationId');
   if (response.message) {
     response.location = '(Not Available)';
   }
   locationElement.textContent = response.location;
-
 }
-// getData() 
+// getData()
